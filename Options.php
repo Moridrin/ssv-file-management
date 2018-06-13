ab26e0580125f9212cc855e8d6bb885e530ca258
JBerkvens <Account@Moridrin.com>
2018-06-13 07:53:14 +0200

    Options in tabs

--- Options.php
+++ Options.php
@@ -10,6 +10,12 @@ abstract class Options
 {
     const OPTION_GROUP = 'mp_ssv_file_manager';
 
+    const SECTIONS = [
+        'connection',
+        'guests',
+        'appearance',
+    ];
+
     const OPTIONS = [
         'connection' => [
             'key'       => [
@@ -36,26 +42,36 @@ abstract class Options
                 'id'          => 'mp_ssv_file_manager__guests_can_view',
                 'title'       => 'Can view Files & Folders',
                 'description' => 'Enabling this allows guests to view files and folders',
+                'type'        => 'boolean',
+                'callback'    => 'showCheckbox',
             ],
             'download' => [
                 'id'          => 'mp_ssv_file_manager__guests_can_download',
                 'title'       => 'Can download Files',
                 'description' => 'Enabling this allows guests to download files',
+                'type'        => 'boolean',
+                'callback'    => 'showCheckbox',
             ],
             'upload'   => [
                 'id'          => 'mp_ssv_file_manager__guests_can_upload',
                 'title'       => 'Can upload Files & Folders',
                 'description' => 'Enabling this allows guests to upload files and folders',
+                'type'        => 'boolean',
+                'callback'    => 'showCheckbox',
             ],
             'edit'     => [
                 'id'          => 'mp_ssv_file_manager__guests_can_edit',
                 'title'       => 'Can edit Files',
                 'description' => 'Enabling this allows guests to edit files',
+                'type'        => 'boolean',
+                'callback'    => 'showCheckbox',
             ],
             'delete'   => [
                 'id'          => 'mp_ssv_file_manager__guests_can_delete',
                 'title'       => 'Can delete Files & Folders',
                 'description' => 'Enabling this allows guests to delete files and folders',
+                'type'        => 'boolean',
+                'callback'    => 'showCheckbox',
             ],
         ],
         'appearance' => [
@@ -80,26 +96,38 @@ abstract class Options
 
     public static function registerSettings()
     {
-        add_settings_section('ssv_connection_section', 'Connection', null, 'ssv_settings');
+        foreach (self::SECTIONS as $section) {
+            add_settings_section('mp_ssv_file_manager_' . $section . '_section', ucfirst($section), null, 'ssv_' . $section);
 
-        foreach (self::OPTIONS['connection'] as $option) {
-            add_settings_field($option['id'], $option['title'], [BaseFunctions::class, 'showTextField'], 'ssv_settings', 'ssv_connection_section', $option);
-            register_setting(self::OPTION_GROUP, $option['id'], ['type' => 'string']);
+            foreach (self::OPTIONS[$section] as $option) {
+                $option += [
+                    'description' => '',
+                    'type'        => 'string',
+                    'callback'    => 'showTextField',
+                ];
+                add_settings_field($option['id'], $option['title'], [BaseFunctions::class, $option['callback']], 'ssv_' . $section, 'mp_ssv_file_manager_' . $section . '_section', $option);
+                register_setting(self::OPTION_GROUP . '_ssv_' . $section, $option['id'], ['type' => $option['type']]);
+            }
         }
 
-        add_settings_section('ssv_guest_section', 'Guests', null, 'ssv_settings');
-
-        foreach (self::OPTIONS['guests'] as $option) {
-            add_settings_field($option['id'], $option['title'], [BaseFunctions::class, 'showCheckbox'], 'ssv_settings', 'ssv_guest_section', $option);
-            register_setting(self::OPTION_GROUP, $option['id'], ['type' => 'boolean']);
-        }
-
-        add_settings_section('ssv_appearance_section', 'Appearance', null, 'ssv_settings');
-
-        foreach (self::OPTIONS['appearance'] as $option) {
-            add_settings_field($option['id'], $option['title'], [BaseFunctions::class, $option['callback']], 'ssv_settings', 'ssv_appearance_section', $option);
-            register_setting(self::OPTION_GROUP, $option['id'], ['type' => 'string']);
-        }
+        // foreach (self::OPTIONS['connection'] as $option) {
+        //     add_settings_field($option['id'], $option['title'], [BaseFunctions::class, 'showTextField'], 'ssv_connection', 'ssv_connection_section', $option);
+        //     register_setting(self::OPTION_GROUP.'_connection', $option['id'], ['type' => 'string']);
+        // }
+        //
+        // add_settings_section('ssv_guest_section', 'Guests', null, 'ssv_guest');
+        //
+        // foreach (self::OPTIONS['guests'] as $option) {
+        //     add_settings_field($option['id'], $option['title'], [BaseFunctions::class, 'showCheckbox'], 'ssv_guest', 'ssv_guest_section', $option);
+        //     register_setting(self::OPTION_GROUP.'_guests', $option['id'], ['type' => 'boolean']);
+        // }
+        //
+        // add_settings_section('ssv_appearance_section', 'Appearance', null, 'ssv_appearance');
+        //
+        // foreach (self::OPTIONS['appearance'] as $option) {
+        //     add_settings_field($option['id'], $option['title'], [BaseFunctions::class, $option['callback']], 'ssv_appearance', 'ssv_appearance_section', $option);
+        //     register_setting(self::OPTION_GROUP.'_appearance', $option['id'], ['type' => 'string']);
+        // }
     }
 
     public static function setupNetworkMenu()
@@ -114,22 +142,21 @@ abstract class Options
 
     public static function testFunction()
     {
-        $activeTab             = $_REQUEST['tab'] ?? 'connection';
-        $connectionLink        = $_GET;
-        $connectionLink['tab'] = 'connection';
-
+        $activeTab = $_REQUEST['tab'] ?? 'ssv_connection';
         ?>
         <h2 class="nav-tab-wrapper">
-            <a href="?page=ch-essentials-options&tab=connection" class="nav-tab <?php echo $activeTab == 'connection' ? 'nav-tab-active' : ''; ?>">Front Page Options</a>
-            <a href="?page=ch-essentials-options&tab=header_options" class="nav-tab <?php echo $activeTab == 'header_options' ? 'nav-tab-active' : ''; ?>">Header Options</a>
+            <a href="?<?= BaseFunctions::escape(BaseFunctions::getCurrentUrlWithArguments(['tab' => 'ssv_connection']), 'attr') ?>" class="nav-tab <?= $activeTab == 'ssv_connection' ? 'nav-tab-active' : ''; ?>">Connection</a>
+            <a href="?<?= BaseFunctions::escape(BaseFunctions::getCurrentUrlWithArguments(['tab' => 'ssv_guests']), 'attr') ?>" class="nav-tab <?= $activeTab == 'ssv_guests' ? 'nav-tab-active' : ''; ?>">Guest</a>
+            <a href="?<?= BaseFunctions::escape(BaseFunctions::getCurrentUrlWithArguments(['tab' => 'ssv_appearance']), 'attr') ?>" class="nav-tab <?= $activeTab == 'ssv_appearance' ? 'nav-tab-active' : ''; ?>">Appearance</a>
+            <a href="https://moridrin.com/plugins/ssv-file-manager" class="nav-tab">Help</a>
         </h2>
         <div class="wrap">
             <h1>Your Plugin Page Title</h1>
             <!--suppress HtmlUnknownTarget -->
             <form method="post" action="options.php">
                 <?php
-                settings_fields(self::OPTION_GROUP);
-                do_settings_sections('ssv_settings');
+                settings_fields(self::OPTION_GROUP . '_' . $activeTab);
+                do_settings_sections($activeTab);
                 submit_button();
                 ?>
         </div>
